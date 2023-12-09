a814acc9cbdb4474b993023a5f9ecf9029bec2b5
MoofyWoofy <59331821+MoofyWoofy@users.noreply.github.com>
2023-12-09 13:08:52 +0800

    added ntfy

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # A simple Program to back up files to Google Drive
 
-Though any files can be backed up, It is created to docker files (docker-compose.yaml) etc.
-Remove all lines with subprocess if you do not intent to use it for docker files.
+Though any files can be backed up, It is created for docker files (docker-compose.yaml) etc.
 
 ## Usage
 rename `.env.sample` to `.env` and rename the variables accordingly
 
 Note the following:
-
-- for the environment variables
+- for environment variables
   - `FILENAME`: The file name will be appended to the file after the date eg `2011-01-01-MYBACKUP.tar.gz`
-  - `DOCKER_CONTAINERS`: Containers to stop before archiving, if `all` is set, it will stop all containers and `none` will not stop any containers 
+  - `ARCHIVE_PATH`: This needs to be absolute path
+  - `DOCKER_CONTAINERS`: Containers to stop before archiving, if `all` is set, it will stop all containers and `none` will not stop any containers (recommended if you are not backing docker containers) 
   - `GNUPG_RECIPIENTS`: Which GPG Key to use for encryption
-  - `BACKUP_COPIES`: How many copies to keep, will delete the rest 
-  - `DATE_FORMAT`: Date format, uses [Python datetime strftime format](https://strftime.org/) 
+  - `BACKUP_COPIES`: How many recent copies to keep, the rest will be deleted 
+  - `DATE_FORMAT`: Date format, uses [Python datetime strftime format](https://strftime.org/)
+  - `NTFY_URL` & `NTFY_AUTH` are both optional
 
-Before running, ensure you have a GPG key, `client.secrets.json` and `credentials.json`, else run `get_gdrive_token.py` to get `credentials.json`
+- Before running, ensure you have a GPG key, `client.secrets.json` and `credentials.json`, else run `get_gdrive_token.py` to get `credentials.json`
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from shutil import make_archive
 import subprocess
 import dotenv
 from os import getenv, remove as os_remove_file
+import os.path
 import gnupg
 from datetime import datetime
 from sys import stderr
+import requests
+from base64 import b64encode
 
 
 def run_docker_commands(command, docker_containers_scoped):
@@ -17,18 +20,50 @@ def run_docker_commands(command, docker_containers_scoped):
         print(err, file=stderr)
 
 
-def send_ntfy():
-    pass
+def get_file_location(file_path) -> str:
+    # returns the location adding the slash / for *nix and \ for Windows
+    return os.path.join(os.path.dirname(__file__), file_path)
+
+
+def humanbytes(B):
+    """Return the given bytes as a human friendly KB, MB, GB, or TB string."""
+    B = float(B)
+    KB = float(1024)
+    MB = float(KB ** 2) # 1,048,576
+    GB = float(KB ** 3) # 1,073,741,824
+    TB = float(KB ** 4) # 1,099,511,627,776
+
+    if B < KB:
+        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+    elif KB <= B < MB:
+        return '{0:.2f} KB'.format(B / KB)
+    elif MB <= B < GB:
+        return '{0:.2f} MB'.format(B / MB)
+    elif GB <= B < TB:
+        return '{0:.2f} GB'.format(B / GB)
+    elif TB <= B:
+        return '{0:.2f} TB'.format(B / TB)
+
+
+def send_ntfy(ntfy: dict, title: str, message: str, tags: str, priority: str):
+    requests.post(ntfy["url"],
+                  data=message.encode(encoding='utf-8'),
+                  headers={
+                      "Title": title,
+                      "Priority": priority,
+                      "Tags": tags,
+                      "Authorization": f"Basic {b64encode(ntfy['auth'].encode()).decode()}"
+                  })
 
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
 
     # Get docker containers
-    docker_containers = getenv("DOCKER_CONTAINERS", "none").split(',')
-    if docker_containers == "none":  # Do not run docker commands
+    docker_containers = getenv("DOCKER_CONTAINERS", ["none"]).split(',')
+    if len(docker_containers) == 1 and docker_containers[0] == "none":  # Do not run docker commands
         docker_containers = False
-    elif docker_containers == "all":  # Get all docker running containers
+    elif len(docker_containers) == 1 and docker_containers[0] == "all":  # Get all docker running containers
         result = subprocess.run(["docker", "ps", "--format", "{{.Names}}"], check=True, capture_output=True)
         docker_containers = result.stdout.decode().splitlines()
 
@@ -49,8 +84,10 @@ if __name__ == '__main__':
 
     # Encrypt Tarball
     gpg = gnupg.GPG()
-    with open(filename, 'rb') as file:
-        status = gpg.encrypt_file(file, getenv("GNUPG_RECIPIENTS").split(","), passphrase=getenv("GNUPG_PASSPHRASE"), output=f"{filename}.gpg")
+    with open(get_file_location(filename), 'rb') as file:
+        status = gpg.encrypt_file(
+            file, getenv("GNUPG_RECIPIENTS").split(","),
+            passphrase=getenv("GNUPG_PASSPHRASE"), output=f"{get_file_location(filename)}.gpg")
 
     try:
         # Connect to Google Drive
@@ -59,7 +96,7 @@ if __name__ == '__main__':
 
         # Upload to Google Drive
         file = drive.CreateFile({'parents': [{'id': getenv("GOOGLE_DRIVE_FOLDER_ID")}]})
-        file.SetContentFile(f"{filename}.gpg")
+        file.SetContentFile(f"{get_file_location(filename)}.gpg")
         file.Upload()
         del file
 
@@ -71,10 +108,27 @@ if __name__ == '__main__':
         for i in range(0, len(file_list) - int(getenv("BACKUP_COPIES"))):
             file_list[i].Delete()
 
-        send_ntfy()  # Send notification NTFY, backup success
+        # Send notification, backup success
+        if getenv("NTFY_URL") and getenv("NTFY_AUTH"):
+            send_ntfy(
+                {"url": getenv("NTFY_URL"), "auth": getenv("NTFY_AUTH")},
+                "Backup Successful",
+                f"{humanbytes(os.path.getsize(get_file_location(filename)+'.gpg'))} Uploaded!",
+                "heavy_check_mark",
+                "low"
+            )
+
     except RefreshError as err:
         print(err, file=stderr)
-        send_ntfy()  # Notify Backup failed
+        # Send notification, Backup failed
+        if getenv("NTFY_URL") and getenv("NTFY_AUTH"):
+            send_ntfy(
+                {"url": getenv("NTFY_URL"), "auth": getenv("NTFY_AUTH")},
+                "Backup Failed",
+                f"{err}",
+                "x",
+                "high"
+            )
     finally:
         # Remove local files
         os_remove_file(filename)
